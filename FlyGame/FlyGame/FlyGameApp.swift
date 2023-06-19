307f92d937831030788f465bd8ad0433f634c7dc
AlexHorobchuk <118340451+AlexHorobchuk@users.noreply.github.com>
2023-06-18 15:39:00 -1000

    Add files via upload

---
+++ FlyGame/FlyGame/FlyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyGameApp.swift
+//  FlyGame
+//
+//  Created by Olha Dzhyhirei on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FlyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
